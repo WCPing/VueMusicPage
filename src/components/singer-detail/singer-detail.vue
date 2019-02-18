7018380c80b9c22a6ee884bb6da54a54bfd85aaf
wuchangping <w84814077@163.com>
2019-02-18 22:05:57 +0800

    歌手详情1

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,21 @@
+<template>
+    <transition name="slide">
+        <div class="singer-detail"></div>
+    </transition>
+
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .slide-enter-active, .slide-leave-active {
+      transition: all 0.3s
+  }
+  .slide-enter, .slide-leave-to {
+      transform: translate3d(100%, 0, 0)
+  }
+</style>
