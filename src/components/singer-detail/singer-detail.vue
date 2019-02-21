f1316e1205a8abc42734094eb32e048f5912db6e
wuchangping <w84814077@163.com>
2019-02-21 21:41:11 +0800

    新增歌手详情api

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,6 +6,8 @@
 </template>
 <script>
 import {mapGetters} from 'vuex'
+import {getSingerDetail} from 'api/singer'
+import {ERR_OK} from 'api/config'
 export default {
   computed: {
     ...mapGetters([
@@ -13,7 +15,21 @@ export default {
     ])
   },
   created () {
+    this._getDetail()
     console.log(this.singer)
+  },
+  methods: {
+    _getDetail () {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res)
+        }
+      })
+    }
   }
 }
 </script>
