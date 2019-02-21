bfc39a7680af2dbac65fa75d2610b9c03718b3b2
wuchangping <w84814077@163.com>
2019-02-21 22:19:38 +0800

    歌手详情样式修改

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -42,4 +42,13 @@ export default {
   .slide-enter, .slide-leave-to {
       transform: translate3d(100%, 0, 0)
   }
+
+  .singer-detail
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    background: $color-background
 </style>
