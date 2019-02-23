b49b42a2fe4c275989d89728fbec3e13176bd51c
wuchangping <w84814077@163.com>
2019-02-23 23:14:20 +0800

    歌手详情

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
     <transition name="slide">
-        <div class="singer-detail"></div>
+        <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
     </transition>
 
 </template>
@@ -8,8 +8,24 @@
 import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
+  components: {
+    MusicList
+  },
   computed: {
+    title () {
+      return this.singer.name
+    },
+    bgImage () {
+      return this.singer.avatar
+    },
     ...mapGetters([
       'singer'
     ])
@@ -26,9 +42,20 @@ export default {
       }
       getSingerDetail(this.singer.id).then(res => {
         if (res.code === ERR_OK) {
-          console.log(res)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach(item => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
@@ -42,13 +69,4 @@ export default {
   .slide-enter, .slide-leave-to {
       transform: translate3d(100%, 0, 0)
   }
-
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
 </style>
