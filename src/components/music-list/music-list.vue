96f398c4aff76c13c3c0e0680d0c1bf4e189db72
wuchangping <w84814077@163.com>
2019-03-06 23:10:34 +0800

    修改歌手详情列表

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -18,8 +18,12 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
+
 export default {
   props: ['bgImage', 'songs', 'title'],
   components: {
@@ -51,7 +55,7 @@ export default {
       let zIndex = 0
       let scale = 1
       let blur = 0
-      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
       const percent = Math.abs(newY / this.imageHeight)
       if (newY > 0) {
         scale = 1 + percent
@@ -69,8 +73,8 @@ export default {
         this.$refs.bgImage.style.height = 0
       }
       this.$refs.bgImage.style.zIndex = zIndex
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
       this.$refs.filter.style['filter'] = `blur(${blur}px)`
     }
   },
