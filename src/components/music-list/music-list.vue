9c61bd7e33e58bb6d324d95f4d3eea44ed2c33d8
wuchangping <w84814077@163.com>
2019-03-10 17:04:34 +0800

    歌手详情

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="music-list">
-        <div class="back">
+        <div class="back" @click="back">
             <i class="icon-back"></i>
         </div>
         <h1 class="title" v-html="title"></h1>
         <div class="bg-image" :style="bgStyle" ref="bgImage">
+          <div class="play-wrapper">
+            <div class="play" v-show="songs.length > 0" ref="playBtn">
+              <i class="icon-play"></i>
+              <span class="text">随机播放全部</span>
+            </div>
+          </div>
             <div class="filter" ref="filter"></div>
         </div>
         <div class="bg-layer" ref="layer"></div>
@@ -12,12 +18,16 @@
           <div class="song-list-wrapper">
             <song-list :songs="songs"></song-list>
           </div>
+          <div class="loading-container" v-show="!songs.length">
+            <loading></loading>
+          </div>
         </scroll>
     </div>
 </template>
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import Loading from 'base/loading/loading'
 import {prefixStyle} from 'common/js/dom'
 
 const RESERVED_HEIGHT = 40
@@ -28,7 +38,8 @@ export default {
   props: ['bgImage', 'songs', 'title'],
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   data () {
     return {
@@ -68,9 +79,11 @@ export default {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style.zIndex = zIndex
       this.$refs.bgImage.style[transform] = `scale(${scale})`
@@ -81,6 +94,9 @@ export default {
   methods: {
     scroll (pos) {
       this.scrollY = pos.y
+    },
+    back () {
+      this.$router.back()
     }
   }
 }
