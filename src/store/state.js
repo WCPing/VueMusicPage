8814fd0a867e377d11d6ed24e9f1f5a538461f19
wuchangping <w84814077@163.com>
2019-03-14 22:27:57 +0800

    修改vuex

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,12 @@
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {}, // 歌手
+  palying: false, // 是否正在播放
+  fullScreen: false, // 是否全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 歌曲顺序列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1 // 当前歌曲索引
 }
 
 export default state
