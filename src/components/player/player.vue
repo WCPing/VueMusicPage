0ac02a07a6ac605a37565d0c0c9ae9b954b87896
wuchangping <w84814077@163.com>
2019-03-17 18:18:14 +0800

    播放器页面

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,20 +1,85 @@
 <template>
     <div class="player" v-show="playList.length">
+      <transition name="normal">
         <div class="normal-player" v-show="fullScreen">
-            播放器
+            <div class="background">
+            <img width="100%" height="100%" :src="currentSong.image">
+          </div>
+          <div class="top">
+            <div class="back" @click="back">
+              <i class="icon-back"></i>
+            </div>
+            <h1 class="title" v-html="currentSong.name"></h1>
+            <h1 class="subtitle" v-html="currentSong.singer"></h1>
+          </div>
+          <div class="middle">
+            <div class="middle-l">
+              <div class="cd-wrapper">
+                <div class="cd">
+                  <img class="image" :src="currentSong.image">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="operators">
+              <div class="icon i-left">
+                <i class="icon-sequence"></i>
+              </div>
+              <div class="icon i-left">
+                <i class="icon-prev"></i>
+              </div>
+              <div class="icon i-center">
+                <i class="icon-play"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon-next"></i>
+              </div>
+              <div class="icon i-right">
+                <i class="icon icon-not-favorite"></i>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="mini-player" v-show="!fullScreen"></div>
+      </transition>
+      <transition name="mini">
+        <div class="mini-player" v-show="!fullScreen" @click="open">
+          <div class="icon">
+            <img width="40" height="40" :src="currentSong.image">
+          </div>
+          <div class="text">
+            <h2 class="name" v-html="currentSong.name"></h2>
+            <p class="desc" v-html="currentSong.singer"></p>
+          </div>
+          <div class="control"></div>
+          <div class="control">
+            <i class="icon-playlist"></i>
+          </div>
+        </div>
+      </transition>
     </div>
 </template>
 <script>
-import {mapGetters} from 'vuex' 
+import {mapGetters, mapMutations} from 'vuex'
 export default {
-    computed: {
-        ...mapGetters([
-            'fullScreen',
-            'playList'
-        ])
+  computed: {
+    ...mapGetters([
+      'fullScreen',
+      'playList',
+      'currentSong'
+    ])
+  },
+  methods: {
+    back () {
+      this.setFullScreen(false)
     },
+    open () {
+      this.setFullScreen(true)
+    },
+    ...mapMutations({
+      setFullScreen: 'SET_FULL_SCREEN'
+    })
+  }
 }
 </script>
 <style lang="stylus" scoped rel="stylesheet/stylus">
@@ -259,4 +324,3 @@ export default {
       transform: rotate(360deg)
 
 </style>
-
