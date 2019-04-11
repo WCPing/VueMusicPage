e1f30e462e21b335a96dbd96ef661809863114a2
wuchangping <w84814077@163.com>
2019-04-11 22:02:19 +0800

    进度条更新

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -30,7 +30,7 @@
             <div class="progress-wrapper">
               <span class="time time-l">{{format(currentTime)}}</span>
               <div class="progress-bar-wrapper">
-                <progress-bar></progress-bar>
+                <progress-bar :percent="percent"></progress-bar>
               </div>
               <span class="time time-l">{{format(currentSong.duration)}}</span>
             </div>
@@ -107,6 +107,9 @@ export default {
     },
     disableCls () {
       return this.songReady ? '' : 'disable'
+    },
+    percent () {
+      return this.currentTime / this.currentSong.duration
     },
     ...mapGetters([
       'fullScreen',
